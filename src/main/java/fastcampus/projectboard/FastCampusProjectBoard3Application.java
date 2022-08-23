4f7b2fd790f44a9652d7695291b68df0b48cfcea
JeonMoo <rjm0710@naver.com>
2022-08-23 23:45:47 +0900

    프로젝트 생성

---
+++ src/main/java/fastcampus/projectboard/FastCampusProjectBoard3Application.java
@@ -0,0 +1,13 @@
+package fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectBoard3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusProjectBoard3Application.class, args);
+    }
+
+}
