814b485b5c7eb8e5c0593189480dab19679e9574
JeonMoo <rjm0710@naver.com>
2022-08-30 22:27:52 +0900

    백업용 push

--- src/main/java/fastcampus/projectboard/domain/Article.java
+++ src/main/java/fastcampus/projectboard/domain/Article.java
@@ -1,16 +1,39 @@
 package fastcampus.projectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "crateBy")
+})
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;   // 제목
-    private String content; // 본문
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt;    // 생성일시
-    private String createdBy;   // 생성자
-    private LocalDateTime modifiedAt;   // 수정일시
-    private String modifiedBy;  // 수정자
+
+    @Setter @Column(nullable = false) private String title;   // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+
+    @Setter private String hashtag; // 해시태그
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;    // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;   // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;   // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  // 수정자
 }
