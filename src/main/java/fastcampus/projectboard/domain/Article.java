cf47d36712bb455480005b54af5eb14c2a8a9130
jm.ryu <jm.ryu@logifocus.co.kr-at-003371765560>
2022-08-31 17:59:49 +0900

    Entity, Repository 푸쉬

--- src/main/java/fastcampus/projectboard/domain/Article.java
+++ src/main/java/fastcampus/projectboard/domain/Article.java
@@ -10,14 +10,17 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
-        @Index(columnList = "createAt"),
-        @Index(columnList = "crateBy")
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
 })
 @Entity
 public class Article {
@@ -32,8 +35,37 @@ public class Article {
 
     @Setter private String hashtag; // 해시태그
 
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;    // 생성일시
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy;   // 생성자
     @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;   // 수정일시
     @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  // 수정자
+
+    protected  Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
