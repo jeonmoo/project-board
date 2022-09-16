b68f6fe4835643581669593bc597d5ce72efd3a6
Ryu <rjm071@naver.com>
2022-09-16 17:54:09 +0900

    백업 push

---
+++ src/main/java/fastcampus/projectboard/service/ArticleService.java
@@ -0,0 +1,24 @@
+package fastcampus.projectboard.service;
+
+import fastcampus.projectboard.domain.type.SearchType;
+import fastcampus.projectboard.dto.ArticleDto;
+import fastcampus.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return List.of();
+    }
+
+}
